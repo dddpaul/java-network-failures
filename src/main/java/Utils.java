266e4adbef107a39306f84fc2fc3c11c8e070df2
Pavel Derendyaev <paul@reic.ru>
2015-02-02 21:31:49 +0300

    Initial commit

---
+++ src/main/java/Utils.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Utils
+{
+    public static String waitForOutput( Process process ) throws IOException
+    {
+        StringBuilder result = new StringBuilder();
+        BufferedReader b = new BufferedReader( new InputStreamReader( process.getInputStream() ) );
+        String line;
+        while( ( line = b.readLine() ) != null ) {
+            result.append( line ).append( System.lineSeparator() );
+        }
+        return result.toString();
+    }
+}
