0e95743727484300193d1a56730acff59e1a480a
Pavel Derendyaev <pderendyaev@smile-net.ru>
2015-04-24 10:41:08 +0300

    Add OUTPUT chain support

--- src/main/java/com/github/dddpaul/IpTables.java
+++ src/main/java/com/github/dddpaul/IpTables.java
@@ -24,48 +24,71 @@ public class IpTables
         UDP
     }
 
+    public static enum Chain
+    {
+        INPUT,
+        OUTPUT
+    }
+
     public static void drop( int port )
     {
-        addTcpRule( port, Target.DROP );
+        addTcpRule( Chain.INPUT, port, Target.DROP );
     }
 
     public static void reject( int port )
     {
-        addTcpRule( port, Target.REJECT );
+        addTcpRule( Chain.INPUT, port, Target.REJECT );
     }
 
     public static void allow( int port )
     {
         for( Target target : Target.values() ) {
-            removeTcpRule( port, target );
+            removeTcpRule( Chain.INPUT, port, target );
         }
     }
 
-    public static void addTcpRule( int port, Target target )
+    public static void drop( Chain chain, int port )
     {
-        addRule( Proto.TCP, port, target );
+        addTcpRule( chain, port, Target.DROP );
     }
 
-    public static void removeTcpRule( int port, Target target )
+    public static void reject( Chain chain, int port )
     {
-        removeRule( Proto.TCP, port, target );
+        addTcpRule( chain, port, Target.REJECT );
     }
 
-    public static void addRule( Proto proto, int port, Target target )
+    public static void allow( Chain chain, int port )
     {
-        iptables( proto, port, target, true );
+        for( Target target : Target.values() ) {
+            removeTcpRule( chain, port, target );
+        }
     }
 
-    public static void removeRule( Proto proto, int port, Target target )
+    public static void addTcpRule( Chain chain, int port, Target target )
     {
-        iptables( proto, port, target, false );
+        addRule( Proto.TCP, chain, port, target );
     }
 
-    public static void iptables( Proto proto, int port, Target target, boolean add )
+    public static void removeTcpRule( Chain chain, int port, Target target )
+    {
+        removeRule( Proto.TCP, chain, port, target );
+    }
+
+    public static void addRule( Proto proto, Chain chain, int port, Target target )
+    {
+        iptables( proto, chain, port, target, true );
+    }
+
+    public static void removeRule( Proto proto, Chain chain, int port, Target target )
+    {
+        iptables( proto, chain, port, target, false );
+    }
+
+    public static void iptables( Proto proto, Chain chain, int port, Target target, boolean add )
     {
         try {
             String action = add ? "-A" : "-D";
-            List<String> cmd = Arrays.asList( "sudo", "iptables", action, "INPUT", "-p", proto.toString(), "--dport", String.valueOf( port ), "-j", target.toString() );
+            List<String> cmd = Arrays.asList( "sudo", "iptables", action, chain.toString(), "-p", proto.toString(), "--dport", String.valueOf( port ), "-j", target.toString() );
             Process process = new ProcessBuilder( cmd ).redirectErrorStream( true ).start();
             if( process.waitFor() > 0 ) {
                 log.warn( Utils.waitForOutput( process ) );
